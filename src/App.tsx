e9b80b8d667a4127d1d1d45a7b2df4f3ca4ae253
MartinAntonio123 <mavp98@hotmail.com>
2025-02-18 21:09:56 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        ¡Tailwind está funcionando! 🎉
+      </h1>
+    </div>
+  );
+};
+
+export default App;
